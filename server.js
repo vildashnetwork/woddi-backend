b21d9dd7f4f5548daf8b37e71af7278693cdf49e
Li Blissz <liblissz3@gmail.com>
2026-01-19 15:07:11 +0100

    cors configured

--- server.js
+++ server.js
@@ -73,7 +73,8 @@ const allowedOrigins = [
     FRONTEND_URL,
     "http://localhost:8081",
     "http://localhost:8082",
-    "https://woddi-dashboard.onrender.com"
+    "https://woddi-dashboard.onrender.com",
+    "https://woddi.onrender.com"
 ];
 
 
